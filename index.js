a4987dbb307cb8fc2154024a150e324394d231a7
drajat824 <drajat.fikri24@gmail.com>
2023-09-14 14:34:32 +0700

    cors

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const cors = require("cors")
 
 const app = express();
 
+const server = require('http').createServer(app);
+
 app.use("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Sregep is online" });
@@ -21,7 +24,7 @@ require("./routes/student.js")(app);
 require("./routes/teacher.js")(app);
 // db.sequelize.sync();
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log("Server running");
 });
 
